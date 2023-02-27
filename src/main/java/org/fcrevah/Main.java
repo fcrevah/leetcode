7e96f8ca351d76e31f5ee29361177aca96dfdb6e
Yechiel Krakauer <yechielkrakauer@Yechiels-MacBook-Air.local>
2023-02-27 12:51:18 +0200

    Contains duplicate

---
+++ src/main/java/org/fcrevah/Main.java
@@ -0,0 +1,11 @@
+package org.fcrevah;
+
+import org.fcrevah.arrays.ContainsDuplicate;
+
+public class Main {
+    public static void main(String[] args) {
+
+        boolean containsDuplicate = ContainsDuplicate.containsDuplicate_alternate(new int[] {1, 2, 9, 4});
+        System.out.println(containsDuplicate);
+    }
+}
---
+++ src/main/java/org/fcrevah/arrays/ContainsDuplicate.java
@@ -0,0 +1,24 @@
+package org.fcrevah.arrays;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class ContainsDuplicate {
+    public static boolean containsDuplicate(int[] originalArray) {
+        Set<Integer> numSet = new HashSet<>(IntStream.of(originalArray).boxed().collect(Collectors.toSet()));
+        return numSet.size() != originalArray.length;
+    }
+
+    public static boolean containsDuplicate_alternate(int[] originalArray) {
+        Set<Integer> numSet = new HashSet<>();
+        for (int i: originalArray) {
+            if (numSet.contains(i))
+                return true;
+            numSet.add(i);
+        }
+        return false;
+    }
+}
